fd406053d7b1a311c8507188a4efe26c9cb1214d
FrontierPsycho <nonsenseisay@protonmail.com>
2024-10-13 16:59:56 +0200

    Added rubocop.

--- netrunner_probability_utils.rb
+++ netrunner_probability_utils.rb
@@ -13,16 +13,14 @@ module NetrunnerProbabilityUtils
   end
 
   def self.split_probability(success_prob, nested_success_prob, nested_failure_prob)
-    success_prob*nested_success_prob + (1 - success_prob)*nested_failure_prob
+    success_prob * nested_success_prob + (1 - success_prob) * nested_failure_prob
   end
 
   class DeckState
     attr_reader :cards_left, :agendas_left
 
     def initialize(cards_left, agendas_left)
-      if agendas_left > cards_left
-        raise ArgumentError.new("There can't be more agendas than cards left in the deck")
-      end
+      raise ArgumentError, "There can't be more agendas than cards left in the deck" if agendas_left > cards_left
 
       @cards_left = cards_left
       @agendas_left = agendas_left
@@ -40,8 +38,8 @@ module NetrunnerProbabilityUtils
   class RoundResult
     attr_reader :probabilities
 
-    def initialize(o)
-      @probabilities = o.reduce(Hash.new(0.0)) do |acc, outcome|
+    def initialize(outc)
+      @probabilities = outc.each_with_object(Hash.new(0.0)) do |outcome, acc|
         acc[outcome[0]] += outcome[1]
         acc
       end
@@ -54,9 +52,9 @@ module NetrunnerProbabilityUtils
 
     def print
       @probabilities.each do |agendas_stolen, total_probability|
-        puts "Chance to steal %d agendas:\t\t%0.2f%%" % [agendas_stolen, total_probability * 100.0]
+        puts format("Chance to steal %d agendas:\t\t%0.2f%%", agendas_stolen, total_probability * 100.0)
       end
-      puts "Total probability: %0.2f" % [@probabilities.values.reduce(:+) * 100]
+      puts format('Total probability: %0.2f', @probabilities.values.reduce(:+) * 100)
     end
   end
 
@@ -64,6 +62,7 @@ module NetrunnerProbabilityUtils
     if deck_state.agendas_left == 0
       return [[0, 1.0]] # no agendas left, there's only one possible outcome, with 100% probability.
     end
+
     range_upper_limit = [deck_state.agendas_left, 1].min
     (0..range_upper_limit).map do |agendas_stolen|
       [
@@ -73,16 +72,17 @@ module NetrunnerProbabilityUtils
           1 - hypg_cdf(agendas_stolen - 1, deck_state.agendas_left, cards, deck_state.cards_left)
         else
           hypg_pdf(agendas_stolen, deck_state.agendas_left, cards, deck_state.cards_left)
-        end,
+        end
       ]
     end
   end
 
   # use max to limit based on clicks
-  def self.deep_dive_outcomes(deck_state, max=2)
+  def self.deep_dive_outcomes(deck_state, max = 2)
     if deck_state.agendas_left == 0
       return [[0, 1.0]] # no agendas left, there's only one possible outcome, with 100% probability.
     end
+
     range_upper_limit = [deck_state.agendas_left, max].min
     (0..range_upper_limit).map do |agendas_stolen|
       [
@@ -92,12 +92,12 @@ module NetrunnerProbabilityUtils
           1 - hypg_cdf(agendas_stolen - 1, deck_state.agendas_left, 8, deck_state.cards_left)
         else
           hypg_pdf(agendas_stolen, deck_state.agendas_left, 8, deck_state.cards_left)
-        end,
+        end
       ]
     end
   end
 
-  def self.super_round_outcomes(deck_state, khusyuk_cards=4)
+  def self.super_round_outcomes(deck_state, khusyuk_cards = 4)
     # puts "Khusyuk run (#{deck_state})"
 
     # Khusyuk followed by one or two Deep Dives.
@@ -110,33 +110,32 @@ module NetrunnerProbabilityUtils
       # puts "\t1st Deep Dive (#{deck_state_after_khusyuk})"
 
       NetrunnerProbabilityUtils.deep_dive_outcomes(deck_state_after_khusyuk)
-        .map { |first_dd_tuple| [first_dd_tuple[0], first_dd_tuple[1]*k_probability] }
-        .flat_map do |first_dd_tuple|
-          fdd_agendas_stolen, fdd_probability = first_dd_tuple
-          deck_state_after_fdd = deck_state_after_khusyuk.steal(fdd_agendas_stolen)
+                               .map { |first_dd_tuple| [first_dd_tuple[0], first_dd_tuple[1] * k_probability] }
+                               .flat_map do |first_dd_tuple|
+        fdd_agendas_stolen, fdd_probability = first_dd_tuple
+        deck_state_after_fdd = deck_state_after_khusyuk.steal(fdd_agendas_stolen)
 
-          # puts "\t\tStole %d (P: %0.2f%%)" % [fdd_agendas_stolen, fdd_probability*100]
+        # puts "\t\tStole %d (P: %0.2f%%)" % [fdd_agendas_stolen, fdd_probability*100]
 
-          # puts "\t\t2nd Deep Dive (#{deck_state_after_fdd})"
+        # puts "\t\t2nd Deep Dive (#{deck_state_after_fdd})"
 
-          # if the first DD stole 2 agendas, we only have one click left. The second one can steal max 1.
-          max = if fdd_agendas_stolen == 2
-                  1
-                else
-                  2
-                end
+        # if the first DD stole 2 agendas, we only have one click left. The second one can steal max 1.
+        max = if fdd_agendas_stolen == 2
+                1
+              else
+                2
+              end
 
-          NetrunnerProbabilityUtils.deep_dive_outcomes(deck_state_after_fdd, max)
-            .map { |second_dd_tuple| [second_dd_tuple[0], second_dd_tuple[1]*fdd_probability] }
-            .map { |second_dd_tuple|
-              sdd_agendas_stolen, sdd_probability = second_dd_tuple
-              deck_state_after_sdd = deck_state_after_fdd.steal(sdd_agendas_stolen)
-              # puts "\t\t\tStole %d (P: %0.2f%%) - total: %d" % [sdd_agendas_stolen, sdd_probability*100, deck_state.agendas_left - deck_state_after_sdd.agendas_left]
+        NetrunnerProbabilityUtils.deep_dive_outcomes(deck_state_after_fdd, max)
+                                 .map { |second_dd_tuple| [second_dd_tuple[0], second_dd_tuple[1] * fdd_probability] }
+                                 .map do |second_dd_tuple|
+          sdd_agendas_stolen, sdd_probability = second_dd_tuple
+          deck_state_after_sdd = deck_state_after_fdd.steal(sdd_agendas_stolen)
+          # puts "\t\t\tStole %d (P: %0.2f%%) - total: %d" % [sdd_agendas_stolen, sdd_probability*100, deck_state.agendas_left - deck_state_after_sdd.agendas_left]
 
-              [deck_state.agendas_left - deck_state_after_sdd.agendas_left, sdd_probability]
-            }
+          [deck_state.agendas_left - deck_state_after_sdd.agendas_left, sdd_probability]
         end
+      end
     end
-
   end
 end
